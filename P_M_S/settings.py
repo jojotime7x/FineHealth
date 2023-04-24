371c8b1d2ecbf7aa8fd917643c2bd33fc325be53
Jojo <jojotime7x@gmail.com>
2023-04-24 15:21:22 +0100

    chagnes

--- P_M_S/settings.py
+++ P_M_S/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'P_app',
     'Patient_Library',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'P_M_S.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
